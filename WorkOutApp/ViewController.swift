b3c3a419aebde65963b2ba601aa854d9ff212b99
havvk01 <havvk01@gmail.com>
2022-10-02 14:38:57 +0300

    Initial Commit

---
+++ WorkOutApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WorkOutApp
+//
+//  Created by Slava Havvk on 02.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
