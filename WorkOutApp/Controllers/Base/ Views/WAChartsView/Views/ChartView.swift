c53f0d182348629fd90829645d372650cd90070e
havvk01 <havvk01@gmail.com>
2023-03-22 16:49:59 +0300

    -- drawChart

--- WorkOutApp/Controllers/Base/ Views/WAChartsView/Views/ChartView.swift
+++ WorkOutApp/Controllers/Base/ Views/WAChartsView/Views/ChartView.swift
@@ -1,4 +1,4 @@
-//
+ //
 //  ChartView.swift
 //  WorkOutApp
 //
@@ -24,7 +24,8 @@ final class ChartView: WABaseView {
     
     func configure(with data: [WAChartsView.Data]) {
         layoutIfNeeded()
-        addDashLines()
+        drawDashLines()
+        drawChart(with: data)
     }
     
 }
@@ -66,13 +67,13 @@ extension ChartView {
 
 private extension ChartView {
     
-    func addDashLines(with counts: Int? = nil) {
+    func drawDashLines(with counts: Int? = nil) {
         (0..<9).map {CGFloat($0)}.forEach {
-            addDashLine(at: bounds.height / 9 * $0)
+            drawDashLine(at: bounds.height / 9 * $0)
         }
     }
     
-    func addDashLine(at yPosition: CGFloat) {
+    func drawDashLine(at yPosition: CGFloat) {
         let startPoint = CGPoint(x: 0, y: yPosition)
         let endPoint = CGPoint(x: bounds.width, y: yPosition)
         
@@ -87,4 +88,34 @@ private extension ChartView {
         
         layer.addSublayer(dashLayer)
     }
+    
+    func drawChart(with data: [WAChartsView.Data]) {
+        guard let maxValue = data.sorted(by: { $0.value > $1.value}).first?.value else { return }
+        let valuePoints = data.enumerated().map { CGPoint(x: CGFloat($0), y: CGFloat($1.value)) }
+        let chartHeight = bounds.height / CGFloat(maxValue + 10)
+        
+        let points = valuePoints.map {
+            let x = bounds.width / CGFloat(valuePoints.count - 1) * $0.x
+            let y = bounds.height - $0.y * chartHeight
+            return CGPoint(x: x, y: y)
+        }
+        
+        let chartPath = UIBezierPath()
+        chartPath.move(to: points[0])
+        points.forEach {
+            chartPath.addLine(to: $0)
+        }
+        
+        let chartLayer = CAShapeLayer()
+        chartLayer.path = chartPath.cgPath
+        chartLayer.fillColor = UIColor.clear.cgColor
+        chartLayer.strokeColor = R.Colors.active.cgColor
+        chartLayer.lineWidth = 3
+        chartLayer.strokeEnd = 1
+        chartLayer.lineCap = .round
+        chartLayer.lineJoin = .round
+        
+        layer.addSublayer(chartLayer)
+    }
 }
+
